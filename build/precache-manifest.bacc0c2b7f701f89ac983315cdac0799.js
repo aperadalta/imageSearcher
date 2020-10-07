42b960f9ed8eb9ce4214f63b537fdfbe85b60e59
Elheyr <adria.peradalta@gmail.com>
2020-10-07 21:03:53 +0200

    commit on branch

---
+++ build/precache-manifest.bacc0c2b7f701f89ac983315cdac0799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e49b5883ee117207d30f88cd75ddb6f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fb312fa9741cb5fae94",
+    "url": "/static/css/main.63fe444c.chunk.css"
+  },
+  {
+    "revision": "d318b949634f10f5f87c",
+    "url": "/static/js/2.faebf696.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.faebf696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb312fa9741cb5fae94",
+    "url": "/static/js/main.522a1759.chunk.js"
+  },
+  {
+    "revision": "fb9fd11dd4383bbdf232",
+    "url": "/static/js/runtime-main.7abbfe9c.js"
+  }
+]);
